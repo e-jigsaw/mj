22a7d790b68ff2257539874089ce8b8bfc6230c5
jigsaw <m@jgs.me>
2024-01-28 07:32:06 +0900

    show pies

--- src/index.tsx
+++ src/index.tsx
@@ -9,13 +9,57 @@ import resvgWasm from "../node_modules/@resvg/resvg-wasm/index_bg.wasm";
 init(await initYoga(yogaWasm));
 await initWasm(resvgWasm);
 
+const pieSize = (str: string) => {
+  let width = 70;
+  let height = 100;
+  if (str.startsWith("_")) {
+    width = 92;
+    height = 77;
+  }
+  if (str.startsWith("=") || str.startsWith("v")) {
+    width = 92;
+    height = 146;
+  }
+  return { width, height };
+};
+
+const piesSize = (matches: string[]) => {
+  let width = 0;
+  let height = 100;
+  for (const val of matches) {
+    const { width: w, height: h } = pieSize(val);
+    width += w;
+    height = Math.max(height, h);
+  }
+  return { width, height };
+};
+
 export default {
   async fetch(request: Request) {
+    const url = new URL(request.url);
+    const { pathname } = url;
+    if (pathname === "/favicon.ico") {
+      return fetch("https://mji.jgs.me/0m.png");
+    }
+    const matches = [...pathname.matchAll(/[\_\=v]?[0-9][mpsz]/g)].map(
+      ([val]) => val
+    );
+    const size = piesSize(matches);
     const svg = await satori(
-      <div style={{ display: "flex" }}>
-        <img src="https://mji.jgs.me/0m.png"></img>
+      <div style={{ display: "flex", alignItems: "flex-end" }}>
+        {matches.map((val, index) => {
+          const { width, height } = pieSize(val);
+          return (
+            <img
+              key={`${val}-${index}`}
+              src={`https://mji.jgs.me/${val}.png`}
+              width={width}
+              height={height}
+            ></img>
+          );
+        })}
       </div>,
-      { width: 70, height: 100, fonts: [] }
+      { width: size.width, height: size.height, fonts: [] }
     );
     const png = new Resvg(svg).render().asPng();
     return new Response(png, {
